a42c4713f4caa0a3c43ac9f34e555c4a266efe4a
hwang <hh698@126.com>
2017-07-26 21:02:50 +0800

    jinxing change

--- app/js/modules/Order.js
+++ app/js/modules/Order.js
@@ -354,8 +354,7 @@ export default React.createClass({
                 </table>
                 <div>
                     <span className="order_detail">详细地址</span>
-                    <input type="text" name="address_1"  width="100%" placeholder="详细地址" className="order_detail_input order_detail_input_addressDetail"
-                           onChange={this.handleChange}/>
+                    <input type="text" name="address_1"  className="order_detail_input order_detail_input_addressDetail"  onChange={this.handleChange}/>
                 </div>
                 <div>
                     {(() => {
